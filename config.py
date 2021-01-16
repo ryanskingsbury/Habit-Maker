c976ec3bf2e5c396f9a5fce1ec9456de5d9e1d1a
Alan Navai <alannavai99@gmail.com>
2021-01-16 11:16:26 -0800

    Login Screen

---
+++ config.py
@@ -0,0 +1,10 @@
+firebaseconfig = {
+'apiKey':"AIzaSyD9y_qFeAWUgwM721RShs564LSqN6DrA80",
+'authDomain':"habit-maker-41a9d.firebaseapp.com",
+'projectId':"habit-maker-41a9d",
+'storageBucket':"habit-maker-41a9d.appspot.com",
+'messagingSenderId':"326669954512",
+'appId':"1:326669954512:web:67f6e903f0c968a9c81dd5",
+'measurementId':"G-BFEF61DXG7"
+}
+
